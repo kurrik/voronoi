9f41c7f13c9e64398e112826793bd6762046400f
Arne Roomann-Kurrik <kurrik@twitter.com>
2012-09-11 15:21:10 -0700

    Add a package description

--- voronoi.go
+++ voronoi.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// An implementation of Fortune's algorithm to get Voronoi edges for a set of
+// points.
 package voronoi
 
 import (
